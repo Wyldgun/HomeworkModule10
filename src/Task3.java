7743ad67f357dc21e28b1e2ef26778a840f25a77
Andrii <wyldgun@gmail.com>
2023-09-17 20:46:23 +0300

    2

--- src/Task3.java
+++ src/Task3.java
@@ -6,12 +6,9 @@ import java.util.*;
 public class Task3 {
     private File file;
     private ArrayList<Object> dataArray = new ArrayList<>();
-
-
     public Task3(String file) {
         this.file = new File(file);
     }
-
     public void wordFrequencyCount() {
         try (FileReader fileReader = new FileReader(file);
              Scanner scanner = new Scanner(fileReader)) {
@@ -25,15 +22,6 @@ public class Task3 {
             System.out.println(ex.getMessage());
         }
     }
-
-    private void wordFrequencySort() {
-        TreeMap<Integer,String> sortedData = new TreeMap<>();
-        for (int i = 0; i < dataArray.size(); i=i+2) {
-            sortedData.put((Integer)dataArray.get(i+1),dataArray.get(i).toString());
-        }
-        System.out.println("sortedData = " + sortedData.descendingMap());
-    }
-
     private void dataArrayFill(String[] lineArray) {
         boolean isScanned = false;
         int frequency = 1;
@@ -52,6 +40,26 @@ public class Task3 {
                 dataArray.remove(dataArray.indexOf(lineArray[i]) + 2);
             }
             frequency = 1;
+        }
+    }
+    private void wordFrequencySort() {
+        Map<String,Integer> myMap = new HashMap<String, Integer>();
+        for (int i = 0; i < dataArray.size(); i=i+2) {
+            myMap.put(dataArray.get(i).toString(),(Integer)dataArray.get(i+1));
+        }
+        MyComparator myComp = new MyComparator(myMap);
+        Map<String,Integer> sortedMap = new TreeMap(myComp);
+        sortedMap.putAll(myMap);
+        System.out.println("sortedMap = " + sortedMap);
+    }
+    class MyComparator implements Comparator{
+        Map map;
+        public MyComparator(Map map) {
+            this.map = map;
+        }
+        @Override
+        public int compare(Object o1, Object o2) {
+            return ((Integer) map.get(o2)).compareTo((Integer) map.get(o1));
         }
     }
 }
